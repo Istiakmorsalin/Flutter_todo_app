6fbba14245c02251cff4df7ea17f258033bb018c
Istiak Morsalin <istiak.morsalin@monstar-lab.com>
2021-04-30 02:34:54 +0600

    ui bottomsheet added

---
+++ lib/screens/add_task_screen.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class AddTaskScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Color(0xff757575),
+      child: Container(
+        padding: EdgeInsets.all(20.0),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(
+              20,
+            ),
+            topRight: Radius.circular(
+              20,
+            ),
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              'Add task',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 30, color: Colors.lightBlueAccent),
+            ),
+            TextField(
+              autofocus: true,
+              textAlign: TextAlign.center,
+            ),
+            TextButton(
+              onPressed: () {},
+              style: ButtonStyle(
+                backgroundColor:
+                    MaterialStateProperty.all<Color>(Colors.lightBlueAccent),
+              ),
+              child: Text(
+                'Add',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todoy_flutter/screens/add_task_screen.dart';
 import 'package:todoy_flutter/widgets/tasks_list.dart';
 
 class TasksScreen extends StatelessWidget {
@@ -9,7 +10,10 @@ class TasksScreen extends StatelessWidget {
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.lightBlueAccent,
         child: Icon(Icons.add),
-        onPressed: () {},
+        onPressed: () {
+          showModalBottomSheet(
+              context: context, builder: (context) => AddTaskScreen());
+        },
       ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
